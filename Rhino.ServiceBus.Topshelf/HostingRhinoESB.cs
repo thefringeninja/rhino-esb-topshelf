a52c0e5e606188d14ee95ebd79b1e83f9541976f
jbraganca <joao.paul@braganca.name>
2011-02-25 17:08:53 -0800

    class to kick off topshelf

---
+++ Rhino.ServiceBus.Topshelf/HostingRhinoESB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Rhino.ServiceBus.Hosting;
+using Topshelf;
+using Topshelf.Configuration.Dsl;
+using Topshelf.Shelving;
+
+namespace Rhino.ServiceBus.Topshelf
+{
+	public class HostingRhinoESB
+	{
+		public static void Start(string[] args)
+		{
+			var cfg = RunnerConfigurator.New(c =>
+			{
+				c.RunAsLocalSystem();
+
+				var bootStrapperTypes = from type in Assembly.GetCallingAssembly().GetTypes()
+				                        where type.IsGenericallyAssignableFrom(typeof (BootstrapConsumer<>))
+				                              && false == type.IsAbstract
+				                              && false == type.IsInterface
+				                              && type.GetConstructor(Type.EmptyTypes) != null
+				                        select type;
+
+				foreach (var bootStrapperType in bootStrapperTypes)
+				{
+					var bootStrapper = (Bootstrapper<DefaultHost>) (bootStrapperType.GetConstructor(Type.EmptyTypes).IsPublic
+					                                                	? Activator.CreateInstance(bootStrapperType)
+					                                                	: Activator.CreateInstance(bootStrapperType, true));
+
+					c.ConfigureService<DefaultHost>(bootStrapper.InitializeHostedService);
+				}
+			});
+			Runner.Host(cfg, args);
+		}
+	}
+}
---
+++ Rhino.ServiceBus.Topshelf/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Rhino.ServiceBus.Topshelf
+{
+	public static class TypeExtensions
+	{
+		public static bool IsGenericallyAssignableFrom(this Type genericType, Type givenType)
+		{
+			var interfaceTypes = givenType.GetInterfaces();
+
+			if (interfaceTypes.Where(it => it.IsGenericType).Any(it => it.GetGenericTypeDefinition() == genericType))
+			{
+				return true;
+			}
+
+			var baseType = givenType.BaseType;
+			if (baseType == null)
+			{
+				return false;
+			}
+
+			if (baseType.IsGenericType && baseType.GetGenericTypeDefinition() == genericType)
+				return true;
+			return genericType.IsGenericallyAssignableFrom(baseType);
+		}
+	}
+}
